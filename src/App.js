f70f9bf7d0e13af27ae00834c32fb831323b386e
umersaeedbajwa <66518793+umersaeedbajwa@users.noreply.github.com>
2020-06-14 12:17:54 +0500

    Make new course file and also update css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import './App.css';
+import New from './course.js';
 
 function App() {
   return (
     <div className="App">
-      <h1>Asslam o Alikum.</h1>
-      <p>Here we are doing to install react. yes</p>
+      <h1>Piaic Courses list</h1>
+      <h2>Piaic offer following 4 courses.</h2>
+      <ol><h3>
+          <li class="Alignment"><New course_no= "1st" course_name= { "Artificial Intelligence" }/></li>
+          <li class="Alignment"><New course_no= "2nd" course_name= { "Cloud Computing" }/></li>
+          <li class="Alignment"><New course_no= "3rd" course_name= { "Blockchain" }/></li>
+          <li class="Alignment"><New course_no= "4th" course_name= { "Internet of things." }/></li> 
+          </h3>
+      </ol>
     </div>
   );
 }
---
+++ src/new.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.css'
+
+function New (props) {
+    return (
+            <div>
+                
+                
+                    <h6>The {props.course_no} course is {props.course_name}</h6>
+
+
+            </div>
+    )   
+}
+
+export default New;
